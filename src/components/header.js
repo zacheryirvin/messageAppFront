531e03cb5ce50199f467de5d59bd24b0f7a76a2a
badCompany55 <zachirvin45@gmail.com>
2020-03-28 21:34:55 -0500

    login config

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,33 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import styled from '@emotion/styled';
+import {css, jsx} from '@emotion/core';
+import PropTypes from "prop-types";
+import React from "react";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const NavLink = styled(Link)`
+  margin-right: 5px;
+`
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+const Header = ({ siteTitle }) => {
+  return (
+    <header>
+      <div css={css`
+        display: flex;
+        justify-content: right;
+      `}>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/login/'>Login</NavLink>
+      </div>
+    </header>
+  )
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+// Header.propTypes = {
+  // siteTitle: PropTypes.string,
+// }
+//
+// Header.defaultProps = {
+  // siteTitle: ``,
+// }
 
-export default Header
+export default Header;
---
+++ src/pages/login.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {useState} from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import styled from '@emotion/styled';
+import {Link, navigate} from 'gatsby';
+import {css, jsx} from '@emotion/core';
+
+const ContainerDiv = styled('div')`
+  width: 300px;
+  margin: 0 auto;
+`
+const FlexDiv = styled('div')`
+  display: flex;
+`
+const ButtonDiv = styled('div')`
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 5px;
+`
+
+const Login = () => {
+
+  const [login, setLogin] = useState({user_name: "", password: ""});
+  const captureLogin = (e) => {
+    let copyLogin = {...login}
+    const target = e.target.id
+    const value = e.target.value
+    copyLogin[target] = value;
+    setLogin(copyLogin);
+  }
+
+  const hitLogin = async (e) => {
+    e.preventDefault();
+    const url = 'http://localhost:4000/users/login';
+    const reset = {user_name: "", password: ""}
+    const res = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(login)})
+    setLogin(reset);
+    const response = await res; 
+    if (response.status === 200) {
+      console.log(response)
+      navigate('/');
+      return response;
+    } else {
+      return response;
+    }
+  }
+
+  return (
+    <Layout>
+      <SEO title="Login" />
+      <div>
+        <ContainerDiv>
+        <FlexDiv>
+          <label css={css`width: 100px`} htmlFor="user_name">Username </label>
+          <input id='user_name' type="text" value={login.user_name} onChange={captureLogin}/>
+        </FlexDiv>
+        </ContainerDiv>
+        <ContainerDiv>
+        <FlexDiv>
+          <label css={css`width: 100px`} htmlFor="password">Password </label>
+          <input id='password' type="password" value={login.password} onChange={captureLogin}/>
+        </FlexDiv>
+        </ContainerDiv>
+        <ButtonDiv>
+          <button css={css`width: 300px`} onClick={hitLogin}>Submit</button>
+        </ButtonDiv>
+      </div>
+    </Layout>
+  )
+}
+
+export default Login;
