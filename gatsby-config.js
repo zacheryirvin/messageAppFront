a2cdf8b2bca63d0e780d927742d868cae6ba8368
zacheryirvin <zachirvin45@gmail.com>
2020-06-09 21:40:46 -0500

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,19 @@ module.exports = {
           }
         ]
       }
-    }
+    },
+	{
+		resolve: `gatsby-plugin-manifest`,
+		options: {
+			name:  `ZMESSAGEAPP`,
+			short_name: `ZMESSAGE`,
+			start_url: `/`,
+			background_color: `#FFF`,
+			display: `standalone`,
+			icon: `./chat.png`
+		}
+	},
+	`gatsby-plugin-offline`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
