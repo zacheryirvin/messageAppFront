dcf35b5049217086ff88aa6302b98d2f7cf908d2
zachery irvin <zachirvin45@gmail.com>
2025-11-16 17:26:39 -0800

    localhost endpoints

--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -33,8 +33,8 @@ const Signup = () => {
   }
 
   const hitLogin = async (e) => {
-    // const url = 'http://localhost:4000/users/register';
-    const url = `${process.env.GATSBY_USERS_URL}/register`
+    const url = 'http://localhost:4000/users/register';
+    //const url = `${process.env.GATSBY_USERS_URL}/register`
     const reset = {first_name: "", last_name: "", user_name: "", email: "", password: ""}
     const res = await fetch(url, {
       headers: {
